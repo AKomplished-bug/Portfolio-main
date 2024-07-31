f66b0f7c35d32d87a63a92986f528a61a7aed59c
Athul <athulkrishna8781@gmail.com>
2024-07-31 15:38:07 +0530

    connected to a vps

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ const SearchComponent = () => {
   const handleSearch = async () => {
     setLoading(true);
     try {
-      const result = await axios.post('http://localhost:5000/api/chat', { query });
+      const result = await axios.post('https://api-athul.albinvar.in/api/chat ', { query });
       setResponse(result.data.response);
     } catch (error) {
       console.error('Error fetching response:', error);
