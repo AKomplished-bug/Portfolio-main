24eeb18c585e8da7d96df86fad3723e1411ae51e
Athul <athulkrishna8781@gmail.com>
2024-08-11 23:33:57 +0530

    updated achievements

--- frontend/src/pages/Achievements.js
+++ frontend/src/pages/Achievements.js
@@ -9,9 +9,6 @@ const Achievements = () => (
         <strong>Spark Venture 2024 Ignite Innovation Challenge:</strong> Participated and won 1st prize, conducted by Toc H Institute of Technology and Freshton Analytics Pvt Ltd, with funding of 1 Lakh INR. The idea was to build a Dyslexic Aid with AI and AR.
       </li>
       <li>
-        <strong>Technical Fest:</strong> Won 1st prize in project presentation. Presented a project on canteen management system.
-      </li>
-      <li>
         <strong>Competitive Coding Competition:</strong> Secured 2nd prize, conducted in online mode by NIT Bhopal.
       </li>
     </ul>
